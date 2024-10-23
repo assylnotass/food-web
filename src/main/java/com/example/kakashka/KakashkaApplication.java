2b1e741baba63445ec801838fd4cfe815216a2ca
assylnotass <assiilzzzh@gmail.com>
2024-10-23 16:00:24 +0500

    first commit

---
+++ src/main/java/com/example/kakashka/KakashkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakashka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakashkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakashkaApplication.class, args);
+	}
+
+}
